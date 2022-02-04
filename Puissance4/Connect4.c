91de992678ca2c79e327af3a3e9a1ec2af695a2f
Julien Doe <jperget@gmail.com>
2022-02-04 06:42:12 +0100

    Initialisation fonction pour afficher la grille

--- Puissance4/Connect4.c
+++ Puissance4/Connect4.c
@@ -1,8 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define NB_LIGNES 7
+#define NB_COLONNES 7
+
+
+void affiche_grille()
+{
+	for (int j = 0; j < NB_COLONNES; j++)
+		printf("  %d ", j + 1);
+	printf("\n+");
+	for (int j = 0; j < NB_COLONNES; j++)
+		printf("---+");
+	printf("\n|");
+
+
+}
 
 int main(int argc, char* argv[])
 {
+	affiche_grille();
 	return 0;
 }
