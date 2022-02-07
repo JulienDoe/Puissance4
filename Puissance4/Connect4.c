9adad933c3a83c9c4c5281243f6ffebc46cb264b
Julien Doe <jperget@gmail.com>
2022-02-07 05:40:03 +0100

    Structure du jeu commencée

--- Puissance4/Connect4.c
+++ Puissance4/Connect4.c
@@ -4,6 +4,8 @@
 #define NB_LIGNES 7
 #define NB_COLONNES 7
 
+#define P1_COIN ('O')
+#define P2_COIN ('X')
 
 void affiche_grille()
 {
@@ -12,13 +14,39 @@ void affiche_grille()
 	printf("\n+");
 	for (int j = 0; j < NB_COLONNES; j++)
 		printf("---+");
-	printf("\n|");
+	for (int i = 0; i < NB_LIGNES; i++)
+	{
+		printf("\n|");
+		for (int j = 0; j < NB_COLONNES; j++)
+		{
+			printf(" ");
+			printf(" "); //afficher à cet endroit les jetons des joueurs 1 ou 2 
+			printf(" |");
+		}
+	}
+	printf("\n+");
+	for (int j = 0; j < NB_COLONNES; j++)
+		printf("---+");
+	printf("\n");
+	for (int j = 0; j < NB_COLONNES; j++)
+		printf("  %d ", j + 1);
+}
 
+void statut_jeu()
+{
 
 }
 
 int main(int argc, char* argv[])
 {
+	
+	printf("\n\n");
 	affiche_grille();
+	printf("\n\n\t Joueur 1 : ");
+	statut_jeu();
+	printf("\n\n");
+	affiche_grille();
+	printf("\n\n\t Joueur 2 : ");
+	statut_jeu();
 	return 0;
 }
